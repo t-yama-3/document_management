17d9f5a7e4b3a50ff90b1c186139b3bf6e54eea3
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-05-05 12:46:36 +0900

    add register function 2

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -9,10 +9,20 @@ class DocumentsController < ApplicationController
   end
 
   def create
+    @document = Document.new(document_params)
+    if @document.save
+      redirect_to document_path(@document.id)
+    else
+      render :new
+    end
   end
 
   def show
     response.headers['X-Frame-Options'] = 'ALLOWALL'
     @document = Document.find(params[:id])
+  end
+  private
+  def document_params
+    params.require(:document).permit(:title, :src, :note, :group_id)
   end
 end
