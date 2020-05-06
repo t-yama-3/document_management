f09c61094940a032b2e88d4a9a331f4cbc20fe1c
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-05-06 12:47:33 +0900

    Delete group model

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'documents#index'
   resources :documents, except: [:index]
-  resources :groups, only: [:show]
   resources :sections, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,16 +18,8 @@ ActiveRecord::Schema.define(version: 2020_05_06_030709) do
     t.string "src"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "group_id"
     t.bigint "section_id"
-    t.index ["group_id"], name: "index_documents_on_group_id"
     t.index ["section_id"], name: "index_documents_on_section_id"
-  end
-
-  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "group_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "sections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -53,7 +45,6 @@ ActiveRecord::Schema.define(version: 2020_05_06_030709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "documents", "groups"
   add_foreign_key "documents", "sections"
   add_foreign_key "sections", "users"
 end
