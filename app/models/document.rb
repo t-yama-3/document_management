6b002ceafaba8a09442461493009aa6b9591c4dc
t-yama-3 <pxbdg597@ybb.ne.jp>
2020-05-07 19:37:26 +0900

    Add search function

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,10 @@
 class Document < ApplicationRecord
-  belongs_to :group, optional: true
   belongs_to :section, optional: true
+  belongs_to :user
+
+  def self.search(search)
+    return Document.all unless search
+    search = "%#{search}%"
+    Document.find_by_sql(["select * from documents where title like ? or note like ?", search, search])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'documents#index'
+  namespace :documents do
+    resources :searches, only: :index
+  end
   resources :documents, except: [:index]
   resources :sections
+  resources :users, only: [:index]
 end
